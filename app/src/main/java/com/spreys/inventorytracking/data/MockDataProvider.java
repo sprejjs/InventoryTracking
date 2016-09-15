761b079afea53711e0c776dc59abc3ffa90a12ac
Vlad Spreys <vlad.spreys@trademe.co.nz>
2016-09-15 17:02:46 +1200

    Initial commit

---
+++ app/src/main/java/com/spreys/inventorytracking/data/MockDataProvider.java
@@ -0,0 +1,8 @@
+package com.spreys.inventorytracking.data;
+
+/**
+ * Created by vspreys on 9/15/16.
+ */
+
+public class MockDataProvider {
+}
---
+++ app/src/main/java/com/spreys/inventorytracking/data/Product.java
@@ -0,0 +1,29 @@
+package com.spreys.inventorytracking.data;
+
+/**
+ * Created by vspreys on 9/15/16.
+ */
+
+public class Product {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+}

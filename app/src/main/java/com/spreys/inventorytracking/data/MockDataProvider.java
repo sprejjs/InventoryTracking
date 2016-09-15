7664f29768e8a1dea6c5b1e8ee40311ca3ad9f99
Vlad Spreys <vlad.spreys@trademe.co.nz>
2016-09-15 17:05:22 +1200

    Adds mock data.

--- app/src/main/java/com/spreys/inventorytracking/data/MockDataProvider.java
+++ app/src/main/java/com/spreys/inventorytracking/data/MockDataProvider.java
@@ -1,8 +1,18 @@
 package com.spreys.inventorytracking.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vspreys on 9/15/16.
  */
 
 public class MockDataProvider {
+    public static List<Product> GetMockData() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product("Headphones", 10, 59.99));
+        products.add(new Product("iPad mini", 15, 99.99));
+        products.add(new Product("iPhone 7", 3, 1100));
+        return products;
+    }
 }
